41f8ff0c301a21f3bcbbdcbb698f1491990d7486
unknown <hendrasimz92@gmail.com>
2020-04-07 11:01:02 +0700

    model, timestamp, constant

---
+++ PortalDesa/PortalDesa/src/main/java/com/example/PortalDesa/model/constant/TablesConstant.java
@@ -0,0 +1,11 @@
+package com.example.PortalDesa.model.constant;
+
+/**
+ * Created by Sogumontar Hendra Simangunsong on 07/04/2020.
+ */
+public interface TablesConstant {
+    public String TABEL_USER ="users";
+    public String TABEL_USER_ROLE ="user_role";
+    public String TABEL_ROLES ="roles";
+    public String TABEL_SEQUENCE_USERS ="sequence_users";
+}
