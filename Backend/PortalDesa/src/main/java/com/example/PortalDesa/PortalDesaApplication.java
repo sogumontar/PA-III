a36ca4a723f1e21b7ad0cb0da770356e482f3943
unknown <hendrasimz92@gmail.com>
2020-04-13 22:38:23 +0700

    try to consume API

---
+++ Backend/PortalDesa/src/main/java/com/example/PortalDesa/PortalDesaApplication.java
@@ -0,0 +1,13 @@
+package com.example.PortalDesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+j
+@SpringBootApplication
+public class PortalDesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalDesaApplication.class, args);
+	}
+
+}
---
+++ Backend/PortalDesa/src/test/java/com/example/PortalDesa/PortalDesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PortalDesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalDesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
