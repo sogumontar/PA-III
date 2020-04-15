b99b374f85cc8035d590c5f37fa2398fc34d40d7
unknown <hendrasimz92@gmail.com>
2020-04-15 09:46:47 +0700

    fixing Kecamatan Repo

--- PortalDesa/PortalDesa/src/main/java/com/example/PortalDesa/controller/KecamatanController.java
+++ PortalDesa/PortalDesa/src/main/java/com/example/PortalDesa/controller/KecamatanController.java
@@ -2,7 +2,9 @@ package com.example.PortalDesa.controller;
 
 import com.example.PortalDesa.controller.route.KecamatanControllerRoute;
 import com.example.PortalDesa.model.Kecamatan;
+import com.example.PortalDesa.model.Users;
 import com.example.PortalDesa.repository.KecamatanRepo;
+import com.example.PortalDesa.repository.UsersRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -10,27 +12,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 /**
  * Created by Sogumontar Hendra Simangunsong on 14/04/2020.
  */
 @Controller
 @RequestMapping(KecamatanControllerRoute.ROUTE_KECAMATAN)
 public class KecamatanController {
+
     @Autowired
     KecamatanRepo kecamatanRepo;
 
     @GetMapping(KecamatanControllerRoute.ROUTE_KECAMATAN_ALL_)
-    public ResponseEntity<?> findAll(){
+    public ResponseEntity<?> findAll() {
         return ResponseEntity.ok(kecamatanRepo.findAll());
     }
 
-    @GetMapping("/{name}")
-    public Kecamatan findByName(@PathVariable String name){
-        return kecamatanRepo.findFirstByNama(name);
+    @GetMapping("/{nama}")
+    public Kecamatan findByName(@PathVariable String nama) {
+        if (kecamatanRepo.existsByNama(nama)) {
+            System.out.println(kecamatanRepo.findFirstByNama(nama));
+            System.out.println("ada boy");
+        }
+        return kecamatanRepo.findFirstByNama(nama);
     }
+
     @GetMapping("/nama/{namaCamat}")
-    public Kecamatan findbyNamaCamat(@PathVariable String namaCamat){
+    public List findbyNamaCamat(@PathVariable String namaCamat) {
         return kecamatanRepo.findFirstByNamaCamat(namaCamat);
     }
 
+    @GetMapping("/sku{sku_kecamatan}")
+    public ResponseEntity<?> findBySku(@PathVariable String sku_kecamatan) {
+        System.out.println("asd");
+        return ResponseEntity.ok(kecamatanRepo.findBySkuKecamatan(sku_kecamatan));
+    }
 }
--- PortalDesa/PortalDesa/src/main/java/com/example/PortalDesa/repository/KecamatanRepo.java
+++ PortalDesa/PortalDesa/src/main/java/com/example/PortalDesa/repository/KecamatanRepo.java
@@ -3,6 +3,9 @@ package com.example.PortalDesa.repository;
 import com.example.PortalDesa.model.Kecamatan;
 import com.example.PortalDesa.model.defaults.KecamatanDefaults;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * Created by Sogumontar Hendra Simangunsong on 14/04/2020.
@@ -10,5 +13,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface KecamatanRepo extends JpaRepository<Kecamatan,String>{
     Boolean existsByNama(String kecamatanDefaults);
     Kecamatan findFirstByNama(String nama);
-    Kecamatan findFirstByNamaCamat(String nama);
+    List findFirstByNamaCamat(String nama);
+
+    @Query("SELECT d FROM Kecamatan d WHERE d.skuKecamatan=?1")
+    Kecamatan findBySkuKecamatan(String sku);
 }
