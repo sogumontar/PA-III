7a227f60a0118d074d7dbd7fb7d4131ba4fc4350
unknown <hendrasimz92@gmail.com>
2020-04-29 12:57:43 +0700

    init to heroku

--- PortalDesa/PortalDesa/src/main/java/com/example/PortalDesa/controller/route/ProdukDesaControllerRoute.java
+++ PortalDesa/PortalDesa/src/main/java/com/example/PortalDesa/controller/route/ProdukDesaControllerRoute.java
@@ -4,7 +4,10 @@ package com.example.PortalDesa.controller.route;
  * Created by Sogumontar Hendra Simangunsong on 19/04/2020.
  */
 public interface ProdukDesaControllerRoute {
+    //forAll
     public String ROUTE_PRODUK_DESA = "/produk";
+
+
     public String ROUTE_PRODUK_DESA_ALL = "/";
     public String ROUTE_PRODUK_DESA_ADD = "/add";
     public String ROUTE_PRODUK_DESA_FIND_BY_SKU = "/sku/{sku}";
